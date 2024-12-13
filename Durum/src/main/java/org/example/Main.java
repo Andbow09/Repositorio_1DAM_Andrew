e1eddf63cadeadfbe058cf06a16c0d2e1f07b969
A720\1dam <andrewbowen0925@gmail.com>
2024-12-13 16:21:02 +0100

    no message

--- Durum/src/main/java/org/example/Main.java
+++ Durum/src/main/java/org/example/Main.java
@@ -1,57 +1,94 @@
 package org.example;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-///**
-// * @author Andrew Bowen
-// * @version 1.0.0
-// */
-
+/**
+ * @author Andrew Bowen
+ * @version 1.0.0
+ */
 public class Main {
     static Scanner in;
 
+    /**
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         in = new Scanner(System.in);
 
-//        System.out.print("Escribe el número de casos de prueba: ");
+        System.out.print("Escribe el número de casos de prueba: ");
         int numCasos;
+
         try {
             numCasos = in.nextInt();
             in.nextLine();
         } catch (InputMismatchException err) {
+            System.out.println("Introduzca un valor válido.");
             return;
         }
+
         for (int i = 0; i < numCasos; i++) {
-            durum();
+            String[] carne = introducirCarne();
+
+            int conejo = 0;
+            int caballo = 0;
+
+            try {
+                conejo = Integer.parseInt(carne[0]);
+                caballo = Integer.parseInt(carne[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("Introduce variables numéricas, por favor. Saliendo...");
+                return;
+            }
+
+            calcularConejo(conejo, caballo);
         }
     }
 
-    public static void durum() {
-//        System.out.print("Introduce la cantidad de carne de conejo y caballo (<conejo> <caballo>): ");
+    /**
+     *
+     */
+    public static String[] introducirCarne() {
+        System.out.print("Introduce la cantidad de carne de conejo y caballo (<conejo> <caballo>): ");
         String carnes = in.nextLine();
+
         String[] carne = carnes.split(" ");
 
-        int conejo = 0;
-        int caballo = 0;
+        for (int i = 0; i < carne.length; i++) {
+            boolean num = carne[i].matches("//d");
 
-        try {
-            conejo = Integer.parseInt(carne[0]);
-            caballo = Integer.parseInt(carne[1]);
-        } catch (NumberFormatException err) {
-//            System.out.println("Introduce variables numéricas, por favor. Saliendo...");
-            return;
+            if (!num) {
+                System.out.println("Introduzca números.");
+                System.exit(0);
+            }
         }
 
+        if (carne.length != 2) {
+            System.out.println("Debe introducir exactamente dos tipos de carne diferentes.");
+            System.exit(0);
+        }
+
+        return carne;
+    }
+
+    /**
+     *
+     * @param conejo
+     * @param caballo
+     */
+    public static void calcularConejo(int conejo, int caballo) {
+
         if((conejo <= 0 || conejo > 100) || (caballo <= 0 || caballo > 100)) {
-//            System.out.print("La cantidad de carne debe ser una razonable, entre 0 y 100. Saliendo...");
+            System.out.print("La cantidad de carne debe ser una razonable, entre 0 y 100. Saliendo...");
             return;
         }
 
         double total = conejo + caballo;
         double porcentaje_conejo = ((double) conejo / total) * 100;
-        int porcentaje = (int) Math.round(porcentaje_conejo);
+        int porcentaje = (int)porcentaje_conejo;
 
-//        System.out.println("Porcentaje de carne de conejo: " + porcentaje);
-//        System.out.println(" ");
+        System.out.println("Porcentaje de carne de conejo: " + porcentaje);
+        System.out.println(" ");
     }
 }
